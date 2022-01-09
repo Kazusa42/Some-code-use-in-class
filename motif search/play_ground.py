0289ab47d83251a3893b09720e7c5033fb311f91
Kitahara Kazusa <94526092+Kazusa42@users.noreply.github.com>
2022-01-09 19:45:02 +0800

    some test code

--- motif search/play_ground.py
+++ motif search/play_ground.py
@@ -1,10 +1,12 @@
 import numpy as np
+import copy
 
 from utils import *
 
+PROTEIN_DATA = r'./proteins.csv'
 # MOTIF_LEN = 10
 
-# protein_list, protein_type = get_proteins(PROTEIN_DATA)
+protein_list, protein_type = get_proteins(PROTEIN_DATA)
 # seq_num, seq_len = protein_list.shape[0], protein_list.shape[1]
 
 # ceta_0 = build_PSSM(protein_list, protein_type)
@@ -26,19 +28,30 @@ from utils import *
 # ceta_0 = build_background(protein_list, protein_type)
 # print(ceta_0)
 
+"""
 protein_type = np.array(['A', 'G', 'C', 'T'])
 motif_list = np.array([['C', 'G', 'G', 'G', 'T', 'A', 'A', 'G', 'T'],
                        ['A', 'A', 'G', 'G', 'T', 'A', 'T', 'G', 'C'],
                        ['C', 'A', 'G', 'G', 'T', 'G', 'A', 'G', 'G']])
 
 pssm = build_PSSM(motif_list, protein_type)
+"""
 
-x = np.array(['A', 'G', 'G', 'C', 'T', 'G', 'A', 'T', 'C'])
-# print(len(x))
-score = 0
-for idx in range(len(x)):
-    curr_c = x[idx]
-    score += pssm[curr_c][idx]
-print(score)
+protein_list = np.array([['A', 'P', 'H', 'I', 'I', 'V', 'A', 'T', 'P', 'G'],
+                         ['G', 'C', 'E', 'I', 'V', 'I', 'A', 'T', 'P', 'G'],
+                         ['G', 'V', 'E', 'I', 'C', 'I', 'A', 'T', 'P', 'G'],
+                         ['G', 'V', 'D', 'I', 'L', 'I', 'G', 'T', 'T', 'G'],
+                         ['R', 'P', 'H', 'I', 'I', 'V', 'A', 'T', 'P', 'G'],
+                         ['K', 'P', 'H', 'I', 'I', 'I', 'A', 'T', 'P', 'G'],
+                         ['K', 'V', 'Q', 'L', 'I', 'I', 'A', 'T', 'P', 'G'],
+                         ['R', 'P', 'D', 'I', 'V', 'I', 'A', 'T', 'P', 'G'],
+                         ['A', 'P', 'H', 'I', 'I', 'V', 'G', 'T', 'P', 'G'],
+                         ['A', 'P', 'H', 'I', 'I', 'V', 'G', 'T', 'P', 'G'],
+                         ['G', 'C', 'H', 'V', 'V', 'I', 'A', 'T', 'P', 'G'],
+                         ['N', 'Q', 'D', 'I', 'V', 'V', 'A', 'T', 'T', 'G']])
 
+mask = np.where(protein_list == 'A', 1, 0)
+print(mask)
 
+x = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(np.sum(x))
