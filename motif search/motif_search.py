af8c18dc14bb4bbc689d3e01c37e3d3533bc3b0b
Kitahara Kazusa <94526092+Kazusa42@users.noreply.github.com>
2022-01-09 19:44:10 +0800

    correct PSSM update rules

--- motif search/motif_search.py
+++ motif search/motif_search.py
@@ -1,23 +1,28 @@
 from utils import *
 
 import numpy as np
+import copy
 
 MOTIF_LEN = 10
 PROTEIN_DATA = r'./proteins.csv'
+BREAK_THRESH = 1e-10
 
 if __name__ == '__main__':
     protein_list, protein_type = get_proteins(PROTEIN_DATA)
     seq_num, seq_len = protein_list.shape[0], protein_list.shape[1]
 
     # init starting point
-    strat_idx = np.random.randint(0, seq_len - MOTIF_LEN)
-    pi = np.array([strat_idx for _ in range(seq_num)]).reshape((seq_num, 1))
+    start_idx = np.random.randint(0, seq_len - MOTIF_LEN)
+    # start_idx = 42
+    print(start_idx)
+    pi = np.array([start_idx for _ in range(seq_num)]).reshape((seq_num, 1))
 
-    sub_protein_list = protein_list[:, strat_idx:strat_idx + MOTIF_LEN]
+    sub_protein_list = protein_list[:, start_idx:start_idx + MOTIF_LEN]
     pssm = build_PSSM(sub_protein_list, protein_type)
-    # last_ceta = ceta.copy()
+    # print(pssm)
+    last_pssm = copy.deepcopy(pssm)
 
-    for cnt in range(400):
+    for cnt in range(200):
         # print(cnt)
         # scan proteins and update ceta
         score_matrix = np.zeros((seq_num, seq_len - MOTIF_LEN))
@@ -31,17 +36,16 @@ if __name__ == '__main__':
                     curr_c = curr_seq[idx2]  # current char in this protein
                     score1 += pssm[curr_c][idx2]
                 score_matrix[idx1][pos] = score1
+
         pi = np.argmax(score_matrix, axis=1).reshape((200, 1))
-        # print(pi[2][0])
-        for i in range(sub_protein_list.shape[0]):
-            sub_protein_list[i] = protein_list[i, pi[i][0]:pi[i][0] + MOTIF_LEN]
-        # print(sub_protein_list.shape)
-        pssm = build_PSSM(sub_protein_list, protein_type)
 
-    print(pi)
-    for key in pssm.keys():
-        print(pssm[key])
+        # update pssm
+        for pos in range(MOTIF_LEN):
+            # possibale protein type occur in this pos
+            poss = protein_list[:, pos:seq_len - MOTIF_LEN + pos]
+            # print(poss)
+            for key in pssm.keys():  # key: current protein type
+                mask = np.where(poss == key, 1, 0)
+                pssm[key][pos] = round(np.sum(mask * score_matrix) / np.sum(score_matrix), 2)
 
-
-
-
+    print_result(pssm, pi)
