c8a1c66ce1e3e7f0405b50c32a66c3dc988965c2
Kitahara Kazusa <94526092+Kazusa42@users.noreply.github.com>
2022-10-20 10:58:06 +0900

    Update java_day1.java

--- java_day1.java
+++ java_day1.java
@@ -50,3 +50,97 @@ public class HelloWorld {
         System.out.println("Hello world!");
     }
 }
+
+public class Student {
+    /* personal information */
+    String stuName, stuId;
+    int stuAge, stuHeight, stuWeight;
+
+    /* basic information */
+    enum Grade {PRIME, MIDDLE, HIGH, COLLEGE}
+    Grade stuGrade;
+    int score1, score2, score3;
+
+    /* Construction method */
+    public Student(String name, String id, int age){
+        stuName = name;
+        stuId = id;
+        stuAge = age;
+        stuGrade = Grade.COLLEGE;
+    }
+
+    /* function */
+    String getStuName() {
+        System.out.println("This is " + stuName + ".\n");
+        return stuName;
+    }
+
+    int getStuAge(){
+        System.out.println("The student is " + stuAge + " years old.\n");
+        return stuAge;
+    }
+
+    int getStuHeight(){
+        System.out.println("The student is " + stuHeight + " cm tall.\n");
+        return stuHeight;
+    }
+
+    int getStuWeight(){
+        System.out.println("The student is " + stuWeight + " kg.\n");
+        return stuWeight;
+    }
+
+    int setScore1(int score) {
+        score1 = score;
+        return score;
+    }
+
+    int setScore2(int score) {
+        score2 = score;
+        return score;
+    }
+
+    int setScore3(int score) {
+        score3 = score;
+        return score;
+    }
+
+    void getAveScore() {
+        float aveScore = ((float)score1 + (float)score2 + (float)score3) / 3;
+        System.out.println("The average score of this student is " + aveScore + ".\n");
+    }
+
+    /* main function */
+    public static void main(String[] args){
+        Student newStudent = new Student("Kazusa", "44211599", 25);
+        newStudent.getStuName();
+        newStudent.setScore1(80);
+        newStudent.setScore2(90);
+        newStudent.setScore3(90);
+        newStudent.getAveScore();
+    }
+
+}
+
+class Circle {
+    final double PI = 3.1415926535;
+    double diameter;
+
+    public Circle(double di) {
+        diameter = di;
+    }
+
+    void getPerimeter(){
+        System.out.println("Perimeter of this circle is " + diameter * PI + ".\n");
+    }
+
+    void getArea(){
+        System.out.println("Area of this circle is " + ((diameter / 2) * (diameter / 2) * PI) + ".\n");
+    }
+
+    public static void main(String[] args) {
+        Circle myCircle = new Circle(10);
+        myCircle.getArea();
+        myCircle.getPerimeter();
+    }
+}
